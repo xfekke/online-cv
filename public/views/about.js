2bf522f071809cf3c470e80b50c22e7742f20cd6
xfekke <fekke4201337@gmail.com>
2024-04-02 19:15:52 +0200

    Update
    Moved about me info to correct js file

--- public/views/about.js
+++ public/views/about.js
@@ -1,15 +1,33 @@
 export default () => /*html*/`
-    <h1>About</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-      
-    
-  <div id="aboutMe">
-    I am 12 years old!!
+  <div id="aboutMeText">
+    <h4>
+      My name is Sebastian and this site will work as my online CV since I am currently looking for an internship in
+      Januari
+      2025 as a software tester. I am planning to add more things as I learn to show my current level of skill. I am an
+      enthusiastic and goal oriented individual who always strives to learn something new. I have a creative and
+      positive
+      attitude that reflects on my work.
+      <br>
+      <br>
+      I’m based in Stockholm where I am studying at KYH to become a software tester. If there is an available spot as an
+      intern, I can be found here:
+
+    </h4>
   </div>
+  <div id="linkLogos">
 
-  </br>
+    <a href="https://github.com/xfekke" target="_blank">
+      <img src="./public/image/github.png" alt="Github-link">
+    </a>
 
-  <div id="mySkills">
-    I like to code and draw and dinosaurs
+    <a href="https://www.linkedin.com/in/sebastian-sernald-49b543287/" target="_blank">
+      <img src="./public/image/linkedIn.png" alt="linkedIn-link">
+    </a>
+
+    <a href="mailto:sebastiansernald98@gmail.com">
+      <img src="./public/image/email.png" alt="Email-link">
+    </a>
+
+
   </div>
 `;
